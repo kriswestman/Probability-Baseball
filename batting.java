a8b73e60ae5ed5bebe6ba71fb9c23b2cb180163b
kriswestman <kris_westman@hotmail.com>
2017-05-12 13:34:15 -0700

    Add files via upload

--- batting.java
+++ batting.java
@@ -9,10 +9,10 @@ public class Batting {
       for (int i = 0; i <= 24; i++) {
          result[i] = "strike";
       for (int i = 1; i <= 4; i++) {
-         result[randomPitch()] = "hit";
+         result[pitch.nextInt(25) + 1] = "hit";
       }
       for (int i = 5; i <= 14; i++) {
-         result[randomPitch()] = "ball";
+         result[pitch.nextInt(25) + 1] = "ball";
       }
       if(result[number] = "hit") {
          return hit();
@@ -21,16 +21,20 @@ public class Batting {
       } else if(result[number] = "strike") {
          return "strike";
       }
-      
-   public int randomPitch() {
-   return pitch.nextInt(25) + 1;
    }
-   
-   public String hit   
-
-
-
-
-
-
+         
+   public String hit() {
+      int randomHit = pitch.nextInt(10) + 1;
+      if(randomHit <= 4) {
+         return "single";
+      } else if(randomHit <= 7) {
+         return "dub";
+      } else if(randomHit = 8) {
+         return "triple";
+      } else if(randomHit > 8) {
+         return "homeRun";
+      }
+   }
+      
+  }    
 }
